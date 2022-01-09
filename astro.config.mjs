b2af82d514f24f1406982a791366f9e7043d1fc9
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-01-08 17:53:14 -0700

    all of the part 2 files

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,5 @@
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
 	// Comment out "renderers: []" to enable Astro's default component support.
-	renderers: [],
+	renderers: ['@astrojs/renderer-react', '@astrojs/renderer-svelte'],
 });
